cb47e4fcee71fc3f1bc5adf7df5d5998ce4c7cb5
gilze <gilzera.gil@gmail.com>
2019-10-12 03:59:12 -0300

    Tela de materias, falta algumas alterações ainda

--- src/components/pages/materials/NewMaterial.vue
+++ src/components/pages/materials/NewMaterial.vue
@@ -1,98 +1,120 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="form-group col-md-4">
-        <label for="campo1">Descrição* </label>
-        <input type="text" v-model="descricao" required="required" class="form-control" id="descricao">
+      <div class="form-group col-md-6">
+        <label for="material">Material</label>
+        <font color="red">*</font>
+        <input
+          type="text"
+          v-model="material"
+          required="required"
+          class="form-control"
+          placeholder="Digite o nome do Material"
+          id="material"
+        />
       </div>
+
       <div class="form-group col-md-4">
-        <label for="campo2">Campo Dois </label>
-        <input type="text" class="form-control" id="campo2">
+        <label for="classe.material">Classe Material</label>
+        <font color="red">*</font>
+        <select type="text" class="form-control" id="classe.material">
+          <option disabled selected>Selecione uma opção...</option>
+          <option value="ci">Ciência</option>
+          <option value="co">Consumo</option>
+          <option value="ds">Desenvolvimento Cognitivo</option>
+          <option value="ed">Educação Especial</option>
+          <option value="ef">Educação Física</option>
+          <option value="lp">Língua Portuguesa</option>
+          <option value="ma">Matemática</option>
+          <option value="ns">Natureza e Sociedade</option>
+          <option value="ou">Outros</option>
+        </select>
       </div>
-      <div class="form-group col-md-4">
-        <label for="campo3">Campo Três </label>
-        <input type="text" class="form-control" id="campo3">
+
+      <div class="form-group col-md-10">
+        <label for="descricao.material">Descrição do Material</label>
+        <textarea class="form-control" id="descricao.material" rows="3"></textarea>
       </div>
     </div>
-  
+
     <div class="row">
       <div class="form-group col-md-3">
-        <label for="campo4">Campo Quatro </label>
-        <input type="text" class="form-control" id="campo4">
+        <label for="categoria.material">Categoria Material</label>
+        <font color="red">*</font>
+        <select class="custom-select my-1 mr-sm-2" id="categoria.material">
+          <option disabled selected>Selecione uma opção...</option>
+          <option value="co">Consumíveis</option>
+          <option value="du">Duráveis</option>
+          <option value="do">Dourado</option>
+          <option value="ou">Outros</option>
+        </select>
       </div>
       <div class="form-group col-md-3">
-        <label for="campo5">Campo Cinco </label>
-        <input type="text" class="form-control" id="campo5">
+        <label for="embalagem.material">Embalagem Material</label>
+        <font color="red">*</font>
+        <select class="custom-select my-1 mr-sm-2" id="embalagem.material">
+          <option disabled selected>Selecione uma opção...</option>
+          <option value="ca">Caixa</option>
+          <option value="me">Metro</option>
+          <option value="pa">Pacote</option>
+          <option value="un">Unidade</option>
+          <option value="ou">Outros</option>
+        </select>
       </div>
-      <div class="form-group col-md-3">
-        <label for="campo6">Campo Seis </label>
-        <input type="text" class="form-control" id="campo6">
+      <div class="form-group col-md-2">
+        <label for="quantidade">Quantidade</label>
+        <font color="red">*</font>
+        <input type="text" class="form-control" id="quantidade" />
       </div>
-     <div class="form-group col-md-3">
-        <label for="campo7">Campo Sete </label>
-        <input type="text" class="form-control" id="campo7">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="campo8">Campo Oito</label>
-        <input type="text" class="form-control" id="campo8">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="campo6">Campo Nove</label>
-        <input type="text" class="form-control" id="campo9">
-      </div>
-      
-      <div class="form-group col-md-3">
-        <label for="campo10">Campo Dez</label>
-        <input type="text" class="form-control" id="campo10">
-      </div>
-      <div class="form-group col-md-3">
-        <label for="campo11">Campo Onze</label>
-        <input type="text" class="form-control" id="campo11">
-      </div>
-      <div class="form-group col-md-3">
-        <label for="campo12">Campo Doze</label>
-        <input type="text" class="form-control" id="campo12">
-      </div>
-     <div class="form-group col-md-3">
-        <label for="campo13">Campo Treze</label>
-        <input type="text" class="form-control" id="campo13">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="campo14">Campo Quatorze</label>
-        <input type="text" class="form-control" id="campo14">
-      </div>
-      <div class="form-group col-md-6">
-        <label for="campo15">Campo Quinze</label>
-        <input type="text"  class="form-control" id="campo15">
+      <div class="row">
+        
+        <div  class="form-group col-md-4">
+          <label for="local.estoque">Local De Estoque</label>
+          <input type="text" class="form-control" id="local.estoque" />
+        </div>
+        <div class="form-group col-md-6">
+          <label for="campo6">campo seis</label>
+          <input type="text" class="form-control" id="campo6" />
+        </div>
+        <div class="form-group col-md-3">
+          <label for="campo6">Campo Nove</label>
+          <input type="text" class="form-control" id="campo9" />
+        </div>
+
+        <div class="form-group col-md-3">
+          <label for="campo10">Campo Dez</label>
+          <input type="text" class="form-control" id="campo10" />
+        </div>
       </div>
     </div>
     <hr />
-    
+
     <div id="actions" class="row">
-      <div class="col-md-12">
+      <div class="col-md-4">
         <button type="submit" @click="checkForm()" class="btn btn-primary">Salvar</button>
-        <button @click="resetFields()" class="btn btn-default">Limpar</button>
+        <button @click="resetFields()" class="btn btn-link fullLine">Limpar</button>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script>
 export default {
-  data( ) {
+  data() {
     return {
-    descricao: ""
+      material: ""
     };
   },
   methods: {
-   checkForm: () =>{
-      if (this.descricao == "") {
-        alert('A Descrição é obrigatório.');
-      }
+    checkform: () => {
+      //if (this.material ==" ") {
+      this.material = " ";
+      alert("A Descrição é obrigatório.");
+      //}
     },
-    resetFields: () =>{
-      this.descricao = "";
+    resetFields: () => {
+      this.material == "";
+      alert("kkkkk");
     }
   }
 };
--- src/components/pages/materials/SearchMaterial.vue
+++ src/components/pages/materials/SearchMaterial.vue
@@ -1,20 +1,86 @@
 <template>
   <div class="container-fluid">
     <!-- Search bar -->
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="inputGroup-sizing-default">
-          <i class="material-icons">search</i>
-        </span>
-        
-    </div>
-      <input
-        type="text"
-        class="form-control"
-        aria-label="Default"
-        aria-describedby="inputGroup-sizing-default"
-    />
-    </div>
+ <div class="row">
+      <div class="form-group col-md-2">
+         <label for="material">Codigo Material</label>
+        <input type="text"
+          v-model="material"
+          required="required"
+          class="form-control"
+          placeholder="Digitando........."
+          id="material" />
+      </div>
+      <div class="form-group col-md-6" >
+         <label for="material">Nome Material</label>
+        <input type="text"
+          v-model="material"
+          required="required"
+          class="form-control"
+          placeholder="Digite  Material"
+          id="material"  />
+          
+      </div>
+    <div class="form-group col-md-4" >
+       <label for="material">
+         <br/>
+       </label>
+      <br/>
+        <button type="button" class="btn btn-primary md-4" @click="procurarmaterial()">Pesquisar</button>
+ </div>
+ </div>
+    <br/>
+    <table class="table" selectable :select-mode="selectMode">
+      <thead>
+        <tr>
+          <th scope="col">
+            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
+              Data/Hora
+            </a>
+          </th>
+          <th scope="col">
+            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
+              Nome Material
+            </a>
+          </th>
+          <th scope="col">
+            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
+              
+            </a>
+          </th>
+          <th scope="col">
+            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
+              Classe Material
+            </a>
+          </th>
+    <th scope="col">
+            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
+              Categoria Material
+            </a>
+          </th>
+          <th scope="col">
+            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
+              Quantidade Estoque
+            </a>
+          </th>
+          <th>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(agenda ,i) in filtrandoAgenda" :key="i">
+          <td>{{agenda.data}}</td>
+          <td>{{agenda.descricao}}</td>
+          <td>{{agenda.material}}</td>
+          <td>{{agenda.professor}}</td>
+          <td width="9%" align="right">
+            <button type="button" class="btnTable open" @click="show(agenda.id)" ></button>
+            <button type="button" class="btnTable edit" @click="showEdit(agenda.id)" ></button>
+            <button type="button" class="btnTable delete" @click="confirmDelete(agenda.id)" ></button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
     <!-- Search results -->
     <div class="container">
       <table class="table table-hover">
