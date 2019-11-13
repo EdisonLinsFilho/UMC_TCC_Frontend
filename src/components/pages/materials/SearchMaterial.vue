7171a80a8a8a2a78e68dd3db038133bceebda974
gilze <gilzera.gil@gmail.com>
2019-11-13 20:33:27 -0300

    Foi feito ajustes nas modal e a troca de botões por iconese alguns alert.

--- src/components/pages/materials/SearchMaterial.vue
+++ src/components/pages/materials/SearchMaterial.vue
@@ -1,83 +1,139 @@
 <template>
   <div class="container-fluid">
     <!-- Search bar -->
- <div class="row">
+    <div class="row">
       <div class="form-group col-md-2">
-         <label for="material">Codigo Material</label>
-        <input type="text"
+        <label>Codigo Material</label>
+        <input
+          type="text"
           v-model="material"
           required="required"
           class="form-control"
-          placeholder="Digitando........."
-          id="material" />
+          placeholder="Digitando...."
+          id="material"
+        />
       </div>
-      <div class="form-group col-md-6" >
-         <label for="material">Nome Material</label>
-        <input type="text"
+      <div class="form-group col-md-6">
+        <label>Nome Material</label>
+        <input
+          type="text"
           v-model="material"
           required="required"
           class="form-control"
           placeholder="Digite  Material"
-          id="material"  />
-          
+          id="material"
+        />
       </div>
-    <div class="form-group col-md-4" >
-       <label for="material">
-         <br/>
-       </label>
-      <br/>
+      <div class="form-group col-md-4">
+        <label>
+          <br />
+        </label>
+        <br />
         <button type="button" class="btn btn-primary md-4" @click="procurarmaterial()">Pesquisar</button>
- </div>
- </div>
-    <br/>
-    <table class="table" selectable :select-mode="selectMode">
+      </div>
+    </div>
+    <br />
+    <table class="table">
       <thead>
         <tr>
           <th scope="col">
-            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
-              Data/Hora
-            </a>
+            <a
+              style="color: black"
+              class="link-table"
+              data-toggle="collapse"
+              role="text"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >Nome Material</a>
           </th>
           <th scope="col">
-            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
-              Nome Material
-            </a>
+            <a
+              style="color: black"
+              class="link-table"
+              data-toggle="collapse"
+              role="text"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >Classe Material</a>
           </th>
           <th scope="col">
-            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
-              Classe Material
-            </a>
+            <a
+              style="color: black"
+              class="link-table"
+              data-toggle="collapse"
+              role="text"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >Categoria Material</a>
           </th>
           <th scope="col">
-            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
-              Categoria Material
-            </a>
-          </th>
-    <th scope="col">
-            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
-              Quantidade Estoque
-            </a>
+            <a
+              style="color: black"
+              class="link-table"
+              data-toggle="collapse"
+              role="text"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >Quantidade Estoque</a>
           </th>
           <th scope="col">
-            <a style="color: black" class="link-table" data-toggle="collapse" role="text" aria-expanded="false" aria-controls="collapseExample">
-              
-            </a>
+            <a
+              style="color: black"
+              class="link-table"
+              data-toggle="collapse"
+              role="text"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >&emsp;Ações</a>
           </th>
-          <th>
+          <th scope="col">
+            <a
+              style="color: black"
+              class="link-table"
+              data-toggle="collapse"
+              role="text"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >Baixa</a>
           </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td>10/10/1010 10:10</td>
           <td>Material 1</td>
           <td>Matematica</td>
           <td>Duradouro</td>
           <td>10</td>
-          <td width="9%" align="right">
-            <button type="button" class="btnTable open" @click="show()" ></button>
-            <button type="button" class="btnTable edit" @click="showEdit()" ></button>
-            <button type="button" class="btnTable delete" @click="confirmDelete()" ></button>
+          <td width="14%" align="Left">
+            <div class="material-icons" style="cursor: pointer" @click="show()">
+              search&ensp;
+              <span></span>
+              <i></i>
+            </div>
+
+            <div class="material-icons" style="cursor: pointer" @click="showEdit()">
+              edit&ensp;
+              <span></span>
+              <i></i>
+            </div>
+
+            <span class="trash" @click="confirmDelete()">
+              <span></span>
+              <i></i>
+            </span>
+          </td>
+          <td>
+            <div
+              class="material-icons"
+              style="cursor: pointer"
+              data-toggle="modal"
+              data-target=".bd-example-modal-lg"
+              @click="confirmBaixa()"
+            >
+              &ensp;import_export
+              <span></span>
+              <i></i>
+            </div>
           </td>
         </tr>
       </tbody>
@@ -91,118 +147,200 @@
         <tbody></tbody>
       </table>
     </div>
-<!-- PopUp -->
-        <modal name="allPageDisbled" height="auto"	>
-          <div class="borda">
-            <br/>
-           <div class="input-group mb-3">
-              <input type="text" v-model="descricaoMaterial" class="form-control" disabled/>
-            </div >
-            <div class="input-group mb-3">
-              <input type="text" v-model="classeMaterial" class="form-control" disabled/>
-            </div>
-            <div class="input-group mb-3">
-              <input type="text" v-model="categoriaMaterial" class="form-control" disabled/>
-            </div>
-            <div class="input-group mb-3">
-              <input type="text" v-model="embalagemMaterial" class="form-control"  disabled/>
-            </div >
-            <div class="input-group mb-3">
-              <input type="text" v-mask="'#####'" v-model="quantidade" class="form-control"  disabled/>
-            </div >
-            <div class="input-group mb-3">
-              <input type="text" v-model="dataLancamento" class="form-control" disabled/>
-            </div>    
-          </div>
-        </modal>
+    <!-- PopUp -->
+    <modal name="allPageDisbled" height="auto">
+      <br />
+      <hr />
+      <h4 class="modal-title">Visualização de material</h4>
+      <hr />
+      <div class="form-group col-md-12">
+        <label>
+          <h5>Nome</h5>
+        </label>
+        <input type="text" v-model="descricaoMaterial" class="form-control" disabled />
+      </div>
+      <div class="form-group col-md-12">
+        <label>
+          <h5>Classe</h5>
+        </label>
+        <input type="text" v-model="classeMaterial" class="form-control" disabled />
+      </div>
+      <div class="form-group col-md-12">
+        <label>
+          <h5>Categoria</h5>
+        </label>
+        <input type="text" v-model="categoriaMaterial" class="form-control" disabled />
+      </div>
+      <div class="form-group col-md-12">
+        <label>
+          <h5>Embalagem</h5>
+        </label>
+        <input type="text" v-model="embalagemMaterial" class="form-control" disabled />
+      </div>
+      <div class="form-group col-md-12">
+        <label>
+          <h5>Quantidade</h5>
+        </label>
+        <input type="text" v-mask="'#####'" v-model="quantidade" class="form-control" disabled />
+      </div>
+      <div class="form-group col-md-12">
+        <label>
+          <h5>Data</h5>
+        </label>
+        <input type="text" v-model="dataLancamento" class="form-control" disabled />
+      </div>
+    </modal>
 
-        <modal name="allPageEdit" height="auto"	>
-          <div class="borda">
-            <br/>
-           <div class="input-group mb-3">
-              <input type="text" v-model="descricaoMaterial" class="form-control" />
-            </div >
-            <div class="input-group mb-3">
-              <input type="text" v-model="classeMaterial" class="form-control" />
-            </div>
-            <div class="input-group mb-3">
-              <input type="text" v-model="categoriaMaterial" class="form-control" />
-            </div>
-            <div class="input-group mb-3">
-              <input type="text" v-model="embalagemMaterial" class="form-control"  />
-            </div >
-            <div class="input-group mb-3">
-              <input type="text" v-mask="'#####'" v-model="quantidade" class="form-control"  />
-            </div >
-            <div class="input-group mb-3">
-              <input type="text" v-model="dataLancamento" class="form-control" />
-            </div>
-            <div align="center">
-              <button type="button" class="btn btn-link fullLine"  @click="clearModalEdit()">Limpar</button>
-              <button type="button" class="btn btn-primary fullLine" @click="saveEdit()">Salvar</button>
-            </div>    
-          </div>
-        </modal>
+    <modal name="allPageEdit" height="auto">
+      <hr />
+      <h4 class="modal-title">Editar material</h4>
+      <div class="modal-fade">
+        <hr />
+        <br />
+        <div class="form-group col-md-12">
+          <label>
+            <h5>Nome</h5>
+          </label>
+          <input type="text" v-model="descricaoMaterial" class="form-control" />
+        </div>
+        <div class="form-group col-md-12">
+          <label>
+            <h5>Classe</h5>
+          </label>
+          <input type="text" v-model="classeMaterial" class="form-control" />
+        </div>
+        <div class="form-group col-md-12">
+          <label>
+            <h5>Categoria</h5>
+          </label>
+          <input type="text" v-model="categoriaMaterial" class="form-control" />
+        </div>
+        <div class="form-group col-md-12">
+          <label>
+            <h5>Embalagem</h5>
+          </label>
+          <input type="text" v-model="embalagemMaterial" class="form-control" />
+        </div>
+        <div class="form-group col-md-12">
+          <label>
+            <h5>Quantidade</h5>
+          </label>
+          <input type="text" v-mask="'#####'" v-model="quantidade" class="form-control" />
+        </div>
+        <div class="form-group col-md-12">
+          <label>
+            <h5>Data</h5>
+          </label>
+          <input type="text" v-model="dataLancamento" class="form-control" />
+        </div>
+        <div align="center">
+          <button type="button" class="btn btn-link fullLine" @click="clearModalEdit()">Limpar</button>
+          <button type="button" class="btn btn-primary fullLine" @click="saveEdit()">Salvar</button>
+        </div>
+      </div>
+    </modal>
 
-        <modal name="confirmDelete" height="auto"	>
-          <div class="borda">
-            <br/>
-            <p class="size" align="center">Deseja realmente excluir ?</p>
-            <div align="center">
-              <button type="button" class="btn btn-link fullLine"  @click="hideDelete()">Cancelar</button>
-              <button type="button" class="btn btn-primary fullLine" @click="saveDelete()">Sim</button>
-            </div>  
-          </div>
-        </modal>
-        <!-- PopUp -->
+    <modal name="confirmDelete" height="auto">
+      <hr />
+      <br />
+      <p class="size" align="center">Deseja realmente excluir ?</p>
+      <div align="center">
+        <hr />
+        <button type="button" class="btn btn-link fullLine" @click="hideDelete()">Cancelar</button>
+        <button type="button" class="btn btn-primary fullLine" @click="saveDelete()">Sim</button>
+      </div>
+    </modal>
+    <!--aqui a modal de Baixa-->
+    <modal name="confirmBaixa" height="auto">
+      <hr />
+      <h4 class="modal-title">Baixa Material</h4>
+      <hr />
+      <br />
+      <div class="form-group col-md-6">
+        <input type="text" v-model="descricaoMaterial" class="form-control" disabled />
+      </div>
+      <div class="form-group col-md-6">
+        <label>
+          <h5>Quantidade em estoque</h5>
+        </label>
+        <input type="text" v-mask="'#####'" v-model="quantidade" class="form-control" />
+      </div>
+      <br />
+      <div class="form-group col-md-6">
+        <label>
+          <h5>Quantidade utilizada</h5>
+        </label>
+        <input type="text" v-model="quantidadeUtilizada" class="form-control" />
+      </div>
+      <br />
+      <div align="center">
+        <button type="button" class="btn btn-success fullLine" @click="baixaMaterial()">Baixar</button>
+        <button type="button" class="btn btn-primary fullLine" @click="saveBaixa()">Salvar</button>
+      </div>
+    </modal>
 
+    <!-- PopUp -->
   </div>
-  
-
 </template>
+
 
 <script>
 export default {
   data() {
     return {
-        descricaoMaterial: "Material 1",
-        classeMaterial: "Matematica",
-        categoriaMaterial: "Duradouro",
-        embalagemMaterial: "Embalagem 1",
-        quantidade: "10",
-        dataLancamento: "10/10/10 10:10"
-      
-
+      descricaoMaterial: "Material 1",
+      classeMaterial: "Matematica",
+      categoriaMaterial: "Duradouro",
+      embalagemMaterial: "Embalagem 1",
+      quantidade: "10",
+      quantidadeUtilizada: "",
+      dataLancamento: "10/10/10 10:10",
+      id: ""
     };
   },
+
   computed: {},
 
   mounted() {},
 
   methods: {
-    hideDelete(){
-      this.$modal.hide('confirmDelete');
+    hideDelete() {
+      this.$modal.hide("confirmDelete");
     },
-    saveDelete(){
+    saveDelete() {
       //Enviar ID para BE para salvar
-
     },
     confirmDelete() {
-      this.$modal.show('confirmDelete');
-      this.id = id;
+      this.$modal.show("confirmDelete");
     },
-    show () {
+    show() {
       this.isEdit = "true";
-      this.$modal.show('allPageDisbled');
+      this.$modal.show("allPageDisbled");
     },
-    showEdit () {
+    showEdit() {
       this.isEdit = "false";
-      this.$modal.show('allPageEdit');
+      this.$modal.show("allPageEdit");
 
-    //Recebendo os campos do BE
+      //Recebendo os campos do BE
+    },
+    confirmBaixa() {
+      this.$modal.show("confirmBaixa");
+    },
+    baixaMaterial() {
+      if (this.quantidadeUtilizada != "") {
+        this.quantidade -= this.quantidadeUtilizada;
+
+        if (this.quantidadeUtilizada > this.quantidade) {
+          alert("Quantidade utilizada maior que quantidade do estoque");
+        }
+      }
+    },
+
+    saveBaixa() {
+      if (this.quantidade < 10) {
+        alert("Quantidade do material em estoque, está abaixo da média");
+      }
     }
-
-
   }
 };
 </script>
@@ -214,30 +352,40 @@ i {
 i:hover {
   color: var(--birorange);
 }
-.open{
-  background-color:greenyellow;
+.open {
+  background-color: greenyellow;
   border-radius: 2px;
   padding: 0;
   border: none;
 }
-.edit{
+.edit {
   background-color: blue;
   border-radius: 2px;
   padding: 0;
   border: none;
 }
-.delete{
+.delete {
   background-color: red;
   border-radius: 2px;
   padding: 0;
   border: none;
 }
-.btnTable{
+
+.btnTable {
   height: 20px;
   width: 20px;
 }
-.fullLine{
+.fullLine {
   align-self: center;
-  width: 297px
+  width: 297px;
+}
+.show {
+  cursor: pointer;
+}
+.material-icons:hover {
+  color: red;
+}
+h4.modal-title {
+  text-align: center;
 }
 </style>
